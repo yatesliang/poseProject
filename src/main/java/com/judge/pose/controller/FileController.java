e8d77424075a39127955ca48b5dbe787c73cefe7
永潮 梁 <yates.lyc@gmail.com>
2019-05-05 22:30:55 +0800

    update file interface

---
+++ src/main/java/com/judge/pose/controller/FileController.java
@@ -0,0 +1,28 @@
+package com.judge.pose.controller;
+
+import com.judge.pose.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Controller
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+    @RequestMapping("/upLoadImage")
+    @ResponseBody
+
+    void upLoadImage(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        int exerciseId = Integer.parseInt(request.getParameter("exId"));
+        String filename = request.getParameter("filename");
+        fileService.qiniuUpload(file, exerciseId, filename);
+    }
+
+}
--- src/main/java/com/judge/pose/dao/ExercisesHasImageMapper.java
+++ src/main/java/com/judge/pose/dao/ExercisesHasImageMapper.java
@@ -4,10 +4,15 @@ import com.judge.pose.domain.ExercisesHasImage;
 
 import com.judge.pose.domain.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
 @Component("exercisesHasImageMapper")
 public interface ExercisesHasImageMapper extends Mapper<ExercisesHasImage> {
-
+    @Select("select max(index_) " +
+            "from exercises_has_image " +
+            "where exercises_id = #{exId} and image_id = #{imageId}")
+    Integer getMaxIndex(@Param("exId") Integer exId, @Param("imageId") Integer imageId);
 }
