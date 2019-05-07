209c52c17f1cd0a0d18016d5bb312fb1d94211ec
永潮 梁 <yates.lyc@gmail.com>
2019-05-07 19:49:36 +0800

    add cross-site access configuration

---
+++ src/main/java/com/judge/pose/config/CrosAccessConfig.java
@@ -0,0 +1,27 @@
+package com.judge.pose.config;
+
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CrosAccessConfig {
+    @Bean
+    public WebMvcConfigurer crosConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*")
+                        .allowCredentials(true)
+                ;
+            }
+        };
+    }
+}
